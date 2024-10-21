c63971db376f8f04d19d3c525e58935b97e71b2d
Marie-Astrid Buelens <mabuelens@gmail.com>
2024-10-21 16:17:19 +0200

    stars Counter - collider Disappear - invulnerability

--- Assets/Scripts/LifeManager.cs
+++ Assets/Scripts/LifeManager.cs
@@ -7,20 +7,17 @@ using UnityEngine;
 public class LifeManager : MonoBehaviour
 {
     [SerializeField] private GameObject prefab;
+    [SerializeField] private float invulnerableCounter = 10f;
+    private bool invulnerable = false;
 
     public int livesNumber = 3;
     private static List<GameObject> stars = new List<GameObject>();
 
-    void Awake(){
-        for (int i = 0; i < livesNumber; i++){
-            stars.Add(prefab);
-        }
-    }
 
     void Start(){
         for (int i = 0; i < livesNumber; i++){
             Vector3 starPos = new Vector3(i, 0, 0);
-            Instantiate(stars[i], transform.position + starPos, transform.rotation);
+            stars.Add(Instantiate(prefab, transform.position + starPos, transform.rotation));
             stars[i].SetActive(true);
         }
     }
@@ -28,18 +25,30 @@ public class LifeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(invulnerable){
+            if(invulnerableCounter > 10){
+                invulnerableCounter -= Time.deltaTime;
+                Debug.Log("Invulnérable !");
+            }
+            else{
+                invulnerable = false;
+                invulnerableCounter = 10f;
+                
+            }
+        }
         
+        for (int i = 0; i < stars.Count; i++){
+            stars[i].SetActive(i < livesNumber);
+        }
         Debug.Log(livesNumber);
     }
 
     void OnTriggerEnter(Collider other){
         int counter = livesNumber - 1;
-        if(other.CompareTag("Enemy")){
+        if(other.CompareTag("Enemy") && invulnerable == false){
             livesNumber--;
-            counter--;
-            if(counter >= 0){
-                stars[counter].SetActive(false);
-            }
+            invulnerable = true;
+            other.gameObject.SetActive(false);
         }
     }
 
